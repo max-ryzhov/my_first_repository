287517f4bb1b55892aff27a6ca31629a7b3fabca
max-ryzhov <max.ryzhov12@gmail.com>
2017-09-06 00:03:13 +0300

    играюсь с js

--- first.js
+++ first.js
@@ -1,1 +1,18 @@
-alert('I am - JavaScript!');
+var mess2='I am - JavaScript!!!'// объявляю и задаю значение переменной
+alert (mess2);
+
+var name='Max', surename='Ryzhov', age='27'
+alert (name + surename + age);
+
+var x=1, y=2, z=-3;
+var h=x+ -z*y;
+alert (h)
+
+var a, b, c;
+
+a=1
+b=4
+c=8
+d=3
+
+alert (c%d)
